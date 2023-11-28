ca309ee8284da167ae4f01f9a0eb047a248ed9ae
2dogsandaguy <Neverclear@ymail.com>
2023-11-28 18:04:09 -0500

    add logic to schemas file

--- homework-README/README.md
+++ homework-README/README.md
@@ -84,13 +84,13 @@ In order for this application to use a GraphQL API, you’ll need to refactor th
 
 You’ll need to complete the following tasks in each of these back-end files:
 
-* `auth.js`: Update the auth middleware function to work with the GraphQL API.
+* x `auth.js`: Update the auth middleware function to work with the GraphQL API.
 
-* `server.js`: Implement the Apollo Server and apply it to the Express server as middleware.
+* x`server.js`: Implement the Apollo Server and apply it to the Express server as middleware.
 
 * `Schemas` directory:
 
-  * `index.js`: Export your typeDefs and resolvers.
+  * x `index.js`: Export your typeDefs and resolvers.
 
   * `resolvers.js`: Define the query and mutation functionality to work with the Mongoose models.
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,67 @@
-const { User } = require('../models');
+const { User, AuthenticationError } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return User.find({});
+    me: async (_, __, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You are not logged in');
+    },
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, throw an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, throw an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Invalid credentials');
+      }
+
+      // If email and password are correct, sign the user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    addUser: async (_, { username, email, password }) => {
+      // First, create the user
+      const user = await User.create({ username, email, password });
+      // To reduce friction for the user, immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
+    },
+    saveBook: async (_, { bookInput }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookInput } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You are not logged in');
+    },
+    removeBook: async (_, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You are not logged in');
     },
   },
 };
